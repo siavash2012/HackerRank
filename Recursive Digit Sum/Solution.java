6e5ae42df7400c3360fe5ba613b926d67b493b57
siavash2012 <contactsiavash@gmail.com>
2024-04-05 10:26:47 -0400

    Create Solution.java

---
+++ Recursive Digit Sum/Solution.java
@@ -0,0 +1,43 @@
+import java.io.*;
+import java.util.*;
+
+
+class Result {
+
+    /*
+     * Complete the 'superDigit' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts following parameters:
+     *  1. STRING n
+     *  2. INTEGER k
+     */
+
+    public static int superDigit(String n, int k) {
+    // Write your code here
+    if(n.length()==1)return Integer.valueOf(n);
+    long sum=Arrays.stream(n.split("")).mapToLong(Long::valueOf).sum()*k;
+    return superDigit(String.valueOf(sum),1);
+    }
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        String[] firstMultipleInput = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+        String n = firstMultipleInput[0];
+
+        int k = Integer.parseInt(firstMultipleInput[1]);
+
+        int result = Result.superDigit(n, k);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
