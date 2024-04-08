56f5bec672e3b91e17bafae601d8c5c115c82fed
siavash2012 <contactsiavash@gmail.com>
2024-04-07 22:08:46 -0400

    Create Solution.java

---
+++ Max Array Sum/Solution.java
@@ -0,0 +1,54 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+    
+
+    /*
+     * Complete the 'maxSubsetSum' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts INTEGER_ARRAY arr as parameter.
+     */
+
+    public static int maxSubsetSum(List<Integer> arr) {
+            ArrayList<Integer>sum=new ArrayList<>();
+            sum.add(arr.get(0));
+            sum.add(Math.max(arr.get(0),arr.get(1)));
+            for(int i=2;i<arr.size();++i){
+                sum.add(Math.max(sum.get(i-1),Math.max(arr.get(i),sum.get(i-2)+arr.get(i))));
+            }
+            return sum.get(sum.size()-1);
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+        List<Integer> arr = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+            .map(Integer::parseInt)
+            .collect(toList());
+
+        int res = Result.maxSubsetSum(arr);
+
+        bufferedWriter.write(String.valueOf(res));
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
