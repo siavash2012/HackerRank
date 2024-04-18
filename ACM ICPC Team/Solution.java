a02104ea61324fca1033f6210ac4890abc0c9453
siavash2012 <contactsiavash@gmail.com>
2024-04-18 09:51:13 -0400

    Create Solution.java

---
+++ ACM ICPC Team/Solution.java
@@ -0,0 +1,73 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    /*
+     * Complete the 'acmTeam' function below.
+     *
+     * The function is expected to return an INTEGER_ARRAY.
+     * The function accepts STRING_ARRAY topic as parameter.
+     */
+
+    public static List<Integer> acmTeam(List<String> topic) {
+    // Write your code here
+    var map=new HashMap<Integer,Integer>();
+    for(int i=0;i<topic.size();++i){
+        var a=new BigInteger(topic.get(i),2);
+        for(int j=i+1;j<topic.size();++j){
+            var b=new BigInteger(topic.get(j),2);
+            var or=a.or(b);
+            var orCount=Arrays.stream(or.toString(2).split("")).filter(e->e.equals("1")).count();
+            map.merge((int)orCount,1,Integer::sum);
+        }
+    }
+    var max=Collections.max(map.keySet());
+    return List.of(max,map.get(max));
+
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        String[] firstMultipleInput = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+        int n = Integer.parseInt(firstMultipleInput[0]);
+
+        int m = Integer.parseInt(firstMultipleInput[1]);
+
+        List<String> topic = IntStream.range(0, n).mapToObj(i -> {
+            try {
+                return bufferedReader.readLine();
+            } catch (IOException ex) {
+                throw new RuntimeException(ex);
+            }
+        })
+            .collect(toList());
+
+        List<Integer> result = Result.acmTeam(topic);
+
+        bufferedWriter.write(
+            result.stream()
+                .map(Object::toString)
+                .collect(joining("\n"))
+            + "\n"
+        );
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
