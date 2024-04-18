2df03d4ab82a86a432d3f97e400385d3e5a6b3d2
siavash2012 <contactsiavash@gmail.com>
2024-04-18 16:13:31 -0400

    Create Solution.java

---
+++ Strange Counter/Solution.java
@@ -0,0 +1,57 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+/* Code implemented by Siavash Khalaj (contactsiavash@gmail.com) */
+
+class Result {
+
+    /*
+     * Complete the 'strangeCounter' function below.
+     *
+     * The function is expected to return a LONG_INTEGER.
+     * The function accepts LONG_INTEGER t as parameter.
+     */
+
+    public static long strangeCounter(long t) {
+    // Write your code here
+    long start=3;
+    long time=1;
+    long total=0;
+    while(true){
+        total+=start;
+        if(total>=t)break;
+        time+=start;
+        start*=2;
+    }
+    long remainder=t-time;
+    return  start-remainder;
+   
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        long t = Long.parseLong(bufferedReader.readLine().trim());
+
+        long result = Result.strangeCounter(t);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
