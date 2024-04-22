6d5f5063048e688d589835a8d374d336273fec0a
siavash2012 <contactsiavash@gmail.com>
2024-04-22 19:58:06 -0400

    Create Solution.java

---
+++ Anagram/Solution.java
@@ -0,0 +1,61 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    /*
+     * Complete the 'anagram' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts STRING s as parameter.
+     */
+
+    public static int anagram(String s) {
+    // Write your code here
+    if(s.length()%2!=0)return -1;
+    int mid=s.length()/2;
+    String a=s.substring(0,mid);
+    String b=s.substring(mid);
+    List<Integer> aL=a.chars().boxed().collect(Collectors.toList());
+    List<Integer> bL=b.chars().boxed().collect(Collectors.toList());
+    for(Integer bb:bL){
+        aL.remove(bb);
+    }
+    return aL.size();
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int q = Integer.parseInt(bufferedReader.readLine().trim());
+
+        IntStream.range(0, q).forEach(qItr -> {
+            try {
+                String s = bufferedReader.readLine();
+
+                int result = Result.anagram(s);
+
+                bufferedWriter.write(String.valueOf(result));
+                bufferedWriter.newLine();
+            } catch (IOException ex) {
+                throw new RuntimeException(ex);
+            }
+        });
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
