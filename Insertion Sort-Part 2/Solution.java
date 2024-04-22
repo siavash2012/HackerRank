61b552f03e2812a55893a204308b3bed391ce833
siavash2012 <contactsiavash@gmail.com>
2024-04-22 10:07:35 -0400

    Create Solution.java

---
+++ Insertion Sort-Part 2/Solution.java
@@ -0,0 +1,54 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    /*
+     * Complete the 'insertionSort2' function below.
+     *
+     * The function accepts following parameters:
+     *  1. INTEGER n
+     *  2. INTEGER_ARRAY arr
+     */
+
+    public static void insertionSort2(int n, List<Integer> arr) {
+    // Write your code here
+    for(int i=1;i<arr.size();++i){
+        int temp=arr.get(i);
+        int j=i;
+        while(j>0 && temp<arr.get(j-1)){
+            arr.set(j,arr.get(j-1));
+            --j;  
+        }
+        arr.set(j,temp);
+        System.out.println(arr.stream().map(a->String.valueOf(a)).collect(Collectors.joining(" ")));
+    }
+
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+        List<Integer> arr = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+            .map(Integer::parseInt)
+            .collect(toList());
+
+        Result.insertionSort2(n, arr);
+
+        bufferedReader.close();
+    }
+}
