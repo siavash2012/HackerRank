6b02e790ec21df0829cac5463244a84c95e6ae3a
siavash2012 <contactsiavash@gmail.com>
2024-04-24 20:53:40 -0400

    Create Solution.java

---
+++ Beautiful Pairs/Solution.java
@@ -0,0 +1,61 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    /*
+     * Complete the 'beautifulPairs' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts following parameters:
+     *  1. INTEGER_ARRAY A
+     *  2. INTEGER_ARRAY B
+     */
+
+    public static int beautifulPairs(List<Integer> A, List<Integer> B) {
+    // Write your code here
+    int pairs=0;
+    var mapA=A.stream().collect(Collectors.groupingBy(a->a,Collectors.counting()));
+    var mapB=B.stream().collect(Collectors.groupingBy(a->a,Collectors.counting()));
+    for(var entry:mapA.entrySet()){
+        pairs+=Math.min(entry.getValue(),mapB.getOrDefault(entry.getKey(),0L));
+    }
+    return pairs==A.size()?pairs-1:pairs+1;
+
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+        List<Integer> A = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+            .map(Integer::parseInt)
+            .collect(toList());
+
+        List<Integer> B = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+            .map(Integer::parseInt)
+            .collect(toList());
+
+        int result = Result.beautifulPairs(A, B);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
