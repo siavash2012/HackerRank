29ec0d22fbc2ca89f947540b863b27e588e87afb
siavash2012 <contactsiavash@gmail.com>
2024-04-25 17:30:49 -0400

    Create Solution.java

---
+++ Maximizing XOR/Solution.java
@@ -0,0 +1,50 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+/* Code implemented by Siavash Khalaj (contactsiavash@gmail.com) */
+
+class Result {
+
+    /*
+     * Complete the 'maximizingXor' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts following parameters:
+     *  1. INTEGER l
+     *  2. INTEGER r
+     */
+
+    public static int maximizingXor(int l, int r) {
+    // Write your code here
+    return Integer.parseInt(Integer.toBinaryString(l^r).replaceAll("0","1"),2);
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int l = Integer.parseInt(bufferedReader.readLine().trim());
+
+        int r = Integer.parseInt(bufferedReader.readLine().trim());
+
+        int result = Result.maximizingXor(l, r);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
