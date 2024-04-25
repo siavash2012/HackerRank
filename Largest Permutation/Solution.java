5b966a625fa373687b7d4ba34929847028a99907
siavash2012 <contactsiavash@gmail.com>
2024-04-24 22:44:58 -0400

    Create Solution.java

---
+++ Largest Permutation/Solution.java
@@ -0,0 +1,70 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    /*
+     * Complete the 'largestPermutation' function below.
+     *
+     * The function is expected to return an INTEGER_ARRAY.
+     * The function accepts following parameters:
+     *  1. INTEGER k
+     *  2. INTEGER_ARRAY arr
+     */
+
+    public static List<Integer> largestPermutation(int k, List<Integer> arr) {
+    // Write your code here
+    Map<Integer,Integer>map=new HashMap<>();
+    for(int i=0;i<arr.size();++i){
+        map.put(arr.get(i),i);
+    }
+    for(int i=0;i<arr.size() && k>0;++i){
+        if(! arr.get(i).equals(arr.size()-i)){
+            int index=map.get(arr.size()-i);
+            map.put(arr.get(i),index);
+            Collections.swap(arr,i,index);
+            --k;
+        }
+    }
+    return arr;
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        String[] firstMultipleInput = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+        int n = Integer.parseInt(firstMultipleInput[0]);
+
+        int k = Integer.parseInt(firstMultipleInput[1]);
+
+        List<Integer> arr = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+            .map(Integer::parseInt)
+            .collect(toList());
+
+        List<Integer> result = Result.largestPermutation(k, arr);
+
+        bufferedWriter.write(
+            result.stream()
+                .map(Object::toString)
+                .collect(joining(" "))
+            + "\n"
+        );
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
