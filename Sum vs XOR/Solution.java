eea3acc804d90e680c415bb4d48ca9fd26311a91
siavash2012 <contactsiavash@gmail.com>
2024-04-25 18:28:50 -0400

    Create Solution.java

---
+++ Sum vs XOR/Solution.java
@@ -0,0 +1,47 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+/* Code implemented by Siavash Khalaj (contactsiavash@gmail.com) */
+
+class Result {
+
+    /*
+     * Complete the 'sumXor' function below.
+     *
+     * The function is expected to return a LONG_INTEGER.
+     * The function accepts LONG_INTEGER  n as parameter.
+     */
+
+    public static long sumXor(long n) {
+    // Write your code here
+    long count=Arrays.stream(Long.toBinaryString(n).split("")).filter(a->a.equals("0")).count();
+    return n==0?1:(long) Math.pow(2,count);
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        long n = Long.parseLong(bufferedReader.readLine().trim());
+
+        long result = Result.sumXor(n);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
