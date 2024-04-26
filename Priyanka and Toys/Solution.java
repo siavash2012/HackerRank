a1726f215de67ca0ef3df66dde3bf8c14fea5136
siavash2012 <contactsiavash@gmail.com>
2024-04-26 16:58:44 -0400

    Create Solution.java

---
+++ Priyanka and Toys/Solution.java
@@ -0,0 +1,55 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    /*
+     * Complete the 'toys' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts INTEGER_ARRAY w as parameter.
+     */
+
+    public static int toys(List<Integer> w) {
+    // Write your code here
+    int count=0;
+    while(! w.isEmpty()){
+        int min=Collections.min(w);
+        List<Integer> temp=w.stream().filter(a->min+4<a).collect(Collectors.toList());
+        w=temp;
+        ++count;
+    }
+    return count;
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+        List<Integer> w = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+            .map(Integer::parseInt)
+            .collect(toList());
+
+        int result = Result.toys(w);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
