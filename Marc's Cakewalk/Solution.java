7285c3cd1e9a37241b8264dcfbfa0fd9c09f36f7
siavash2012 <contactsiavash@gmail.com>
2024-04-26 11:29:26 -0400

    Create Solution.java

---
+++ Marc's Cakewalk/Solution.java
@@ -0,0 +1,56 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    /*
+     * Complete the 'marcsCakewalk' function below.
+     *
+     * The function is expected to return a LONG_INTEGER.
+     * The function accepts INTEGER_ARRAY calorie as parameter.
+     */
+
+    public static long marcsCakewalk(List<Integer> calories) {
+    // Write your code here
+    Collections.sort(calories,Comparator.reverseOrder());
+    long miles=0;
+    long multiplier=1;
+    for(int calorie:calories){
+        miles+=(multiplier*calorie);
+        multiplier*=2;
+    }
+    return miles;
+
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+        List<Integer> calorie = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+            .map(Integer::parseInt)
+            .collect(toList());
+
+        long result = Result.marcsCakewalk(calorie);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
